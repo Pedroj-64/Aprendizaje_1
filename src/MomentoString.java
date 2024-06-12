55b31dfa340f378ef2fc34a32b87e8a71c2e1f02
Pedro Jose Soto Rivera <pedroj.sotor@uqvirtual.edu.co>
2024-06-12 17:24:38 -0500

    .

--- src/MomentoString.java
+++ src/MomentoString.java
@@ -4,17 +4,34 @@ public class MomentoString {
     
      charAT(n) "n" siendo una posicion de 0 a la larges de la cadena, este metodo devuelve el caracter de la posicion deseada
 
-     substring(x,y) extrae una subcadena de texto de la cadena de texto dada siendo x desde que parte inicia y la y el final contando 
+     substring(x,y) extrae una subcadena de texto de la cadena de texto dada siendo X desde que parte inicia y la Y el final contando 
      los caracteres desde 0
     
-     equals(cadena) verifica si dos cadenas de texto son iguales
+     equals(cadena) verifica si dos cadenas de texto son iguales,devolviendo true o false
+     equalsIgnoreCase es la variacion sin destingir mayuscula y minuscula
 
      todas tienen la contruccion de objeto.metodo 
-    
-   
     */
     public static void main(String[] args) {
 
+        final String juju="Pedrito";
+        final String bolsita="pepito";
+
+        int escala=juju.length();
+        char caracter=juju.charAt(3);
+        String subcadena=juju.substring(0, 5);
+        boolean igualdad=juju.equals(bolsita);
+        boolean igualdad2=juju.equalsIgnoreCase(bolsita);
+
+
+
+        System.out.println("hola mundo mi nombre es: " + juju);
+
+        System.out.println("Mi nombre tiene " + escala + " caracteres");
+
+        System.out.println("El caracter ubicado en la posicion 3 en mi nombre es: " + caracter);
+
+        System.out.println("Tambien me llaman " + subcadena);
 
 
     }
