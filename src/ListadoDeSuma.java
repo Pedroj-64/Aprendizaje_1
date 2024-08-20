71aa64165cd45bbdf7bfb83e989c872f7e193654
Pedro Jose Soto R. <pedroj.sotor@uqvirtual.edu.co>
2024-08-20 17:26:24 -0500

    .

--- src/ListadoDeSuma.java
+++ src/ListadoDeSuma.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ListadoDeSuma {
-
+    //Main
     public static void main(String[] args) {
         Scanner entrada1 = entrada();
         int usuario = entradaUsuario(entrada1);
@@ -10,19 +10,31 @@ public class ListadoDeSuma {
         String mensaje = generarMensaje(result);
         mostrarMensaje(mensaje);
     }
-
+    /**
+     * Metodo tipo Scanner para utilizarlo por todos los metodos 
+     * @return
+     */
     public static Scanner entrada() {
         Scanner entrada = new Scanner(System.in);
         return entrada;
     }
-
+    /**
+     * Aqui se pide al usuario el numero de grandor del arreglo que quiere
+     * @param entrada
+     * @return
+     */
     public static int entradaUsuario(Scanner entrada) {
         System.out.println("Por favor introduzca la cantidad de espacio que quiera en el arreglo: ");
         int espacio = entrada.nextInt();
 
         return espacio;
     }
-
+    /**
+     * Aqui se crea el arreglo a su vez se le pide al usurio un input de cada numero
+     * @param entrada
+     * @param espacio
+     * @return
+     */
     public static int[] entradaArreglos(Scanner entrada, int espacio) {
         int[] arreglo = new int[espacio];
         for (int i = 0; i < espacio; i++) {
@@ -32,7 +44,11 @@ public class ListadoDeSuma {
         return arreglo;
 
     }
-
+    /**
+     * Aqui se suma todo el arreglo
+     * @param arreglo
+     * @return
+     */
     public static int sumatoriaOne(int[] arreglo) {
         int resultado = 0;
         for (int i = 0; i < arreglo.length; i++) {
@@ -40,12 +56,19 @@ public class ListadoDeSuma {
         }
         return resultado;
     }
-
+    /**
+     * Aqui se genera un mensaje para poderlo imprimir despues
+     * @param resultado
+     * @return
+     */
     public static String generarMensaje(int resultado) {
         String mensaje = ("La sumatoria de todos los numeros del arreglo es: " + resultado);
         return mensaje;
     }
-
+    /**
+     * Se muestra el mensaje sin retorno al ser el final del progama 
+     * @param mensaje
+     */
     public static void mostrarMensaje(String mensaje) {
         System.out.println(mensaje);
     }
